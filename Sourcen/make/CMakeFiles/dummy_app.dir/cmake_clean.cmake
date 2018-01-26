c87617c82a99add584bddda9258cddb2d108fd82
stiefn <pstiefn@gmail.com>
2018-01-26 14:39:37 +0100

    added cmake files

---
+++ Sourcen/make/CMakeFiles/dummy_app.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dummy_app.dir/main.cc.o"
+  "dummy_app.pdb"
+  "dummy_app"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dummy_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
